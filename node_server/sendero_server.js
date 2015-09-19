f6ed40803a253c1cb2f41ffa4b0d45db12771240
Christian Bouvier <cbouvier@trea.uy>
2015-09-19 02:36:30 -0300

    Se configura en el servidor una cola para los eventos interactivos. Se agrega un evento para recibir las interacciones. En el cliente se crea una conexión de prueba.

--- node_server/sendero_server.js
+++ node_server/sendero_server.js
@@ -4,16 +4,71 @@ var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var fs = require('fs');
 var path = require('path');
+var queue = require('amqplib/callback_api');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 require('socket.io-stream')(io);
 
+/***********************************************/
+/* QUEUE https://github.com/squaremo/amqp.node */
+/***********************************************/
+
+// Create queue
+var interactions_queue = 'interactions_queue';
+
+function bail(err) {
+  console.error(err);
+  process.exit(1);
+}
+
+// Publisher
+function publisher(conn, data) {
+  conn.createChannel(on_open);
+  function on_open(err, ch) {
+    if (err != null) bail(err);
+    ch.assertQueue(interactions_queue);
+    ch.sendToQueue(interactions_queue, new Buffer(data));
+  }
+};
+
+// Process the event data 
+function process_data(data){
+	// Do processing...
+	return data;
+}
+
+/***********************************************/
+/***************** MAIN ************************/
+/***********************************************/
 io.on('connection', function(client){
+
   console.log("Connected client...");
-  client.on('sendFrame', function(data){
-    client.broadcast.emit('frame', data);
-  });
+  console.log("EL CLIENTE: ", client);
+
+	/*
+	 * 
+	 */
+	client.on('sendFrame', function(data){
+		client.broadcast.emit('frame', data);
+	});
+
+	/*
+	 * 
+	 */
+	client.on('interaction', function (data) {
+		console.log('Interaction', data);
+
+		// Process data
+		var processed_data = process_data(data)
+
+		// Insert into the queue
+		queue.connect('amqp://localhost', function(err, conn) {
+			if (err != null) bail(err);
+			publisher(conn,processed_data);      
+		});
+	});
+	
 });
 
 app.get('/', function(req, res){
